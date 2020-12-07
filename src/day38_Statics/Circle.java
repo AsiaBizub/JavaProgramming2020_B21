4ad8ddabfcc7dc1ed81f9d4aa0982e2ae0ebc4dc
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-07 09:23:55 -0600

    commit

---
+++ src/day38_Statics/Circle.java
@@ -0,0 +1,22 @@
+package day38_Statics;
+
+public class Circle {
+
+    public double radius, diameter, area, perimeter;
+
+    public void setInfo(double radius){
+        this.radius = radius;
+        diameter = 2 * radius;
+        area = calculateArea();
+        perimeter = calculatePerimeter();
+    }
+
+    public double calculateArea(){
+        return radius * radius * Math.PI;
+    }
+
+    public double calculatePerimeter(){
+        return diameter * Math.PI;
+    }
+
+}
