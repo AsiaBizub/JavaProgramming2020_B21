27ec8650e762e2f3ca03a31eff00ad8e62454bae
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-14 14:39:23 -0600

    commit

---
+++ src/day41_Encapsulation/CircleObject.java
@@ -0,0 +1,20 @@
+package day41_Encapsulation;
+
+public class CircleObject {
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle (4);
+
+
+        //System.out.println(c1.area);
+
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter());
+
+        c1.setRadius(5);
+
+
+
+    }
+
+}
