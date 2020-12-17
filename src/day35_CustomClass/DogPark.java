8e237424b64e87759da4c6a874ae70745dfdcc96
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-17 11:24:14 -0600

    commit

--- src/day35_CustomClass/DogPark.java
+++ src/day35_CustomClass/DogPark.java
@@ -11,16 +11,15 @@ public class DogPark {
         Dog dog2 = new Dog();
         Dog dog3 = new Dog();
         Dog dog4 = new Dog();
-        Dog dog5 = new Dog();
-        Dog dog6 = new Dog();
+
 
         dog1.setInfo("Tima", "York", "small", 'M',
-                "multicolor", LocalDate.of(2010,01,19));
+                "multicolor", LocalDate.of(2010,1,19));
 
         dog1.getInfo();
 
         dog2.setInfo("Balli","Maltese", "small", 'F',
-                "white", LocalDate.of(2019,12,01));
+                "white", LocalDate.of(2019,12,1));
 
         dog2.getInfo();
 
@@ -34,7 +33,7 @@ public class DogPark {
         dog2.drink("Beer");
 
         dog4.setInfo("Misha", "husky", "big", 'M',
-                "chocolate", LocalDate.of(2015, 06, 14));
+                "chocolate", LocalDate.of(2015, 6, 14));
 
 
         Dog[] dogs = {dog1, dog2, dog3, dog4};
