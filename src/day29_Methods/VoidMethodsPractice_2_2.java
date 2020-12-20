665dc30102e892f7e43d48afda3258bf2bea84be
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-20 00:54:42 -0600

    commit

---
+++ src/day29_Methods/VoidMethodsPractice_2_2.java
@@ -0,0 +1,29 @@
+package day29_Methods;
+import java.util.Scanner;
+
+public class VoidMethodsPractice_2_2 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+        sumOf3Nums(10, 20, 30);
+        sumOf3Nums(40, 30, 10);
+
+       //sumOf3Nums(scan.nextInt(), scan.nextInt(), scan.nextInt()); application of scanner to the parameters
+
+
+    }
+
+
+    public static void sumOf3Nums(int n1, int n2, int n3) {
+
+        System.out.println("Sum is: "+ (n1+n2+n3));
+
+
+    }
+
+}
+
+/*
+Write a method that calculates the sum of 3 numbers
+ */
