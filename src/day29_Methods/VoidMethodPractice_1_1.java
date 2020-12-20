b493ed09c0e6e0b0efbcb8e1d1eefcc935c7ff45
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-20 00:28:44 -0600

    commit

--- src/day29_Methods/VoidMethodPractice_1_1.java
+++ src/day29_Methods/VoidMethodPractice_1_1.java
@@ -1,43 +1,50 @@
 package day29_Methods;
 
-/* AM - Access Modifier,
-Specifier
-returnType
-methodName
-(Parameter)
-{ statements to complete the functionality of the method = body
+/* AM - Access Modifier  Specifier  returnType  methodName (Parameter) { statements to complete the functionality of the method = body}
  */
-
 import java.util.Arrays;
 
 public class VoidMethodPractice_1_1 {
 
     public static void main(String[] args) {
 
-        int [] a1 ={ 100, -200, 300, 50, 40, 59, 700, 0, 1, 2 };
-
+        int[] a1 = {100, -200, 300, 50, 40, 59, 700, 0, 1, 2};
         arrayMaxNum(a1); // call method and pass the array through the parameter, which then gets assigned to the int max,
 
-        int [] a2 = {1, 2, 3, 4, 5, 6, 800, 9, 0 , 1, 12, 13, 4};
+        int[] a2 = {1, 2, 3, 4, 5, 6, 800, 9, 0, 1, 12, 13, 4};
         arrayMaxNum(a2);
     }
-
-    public static void arrayMaxNum (int []arr){
+    public static void arrayMaxNum(int[] arr) {
 /*
         Arrays.sort(arr); // sorts in ascending order, last index number is the maximum
         System.out.println("Maximum number: " +arr[arr.length-1]); // maximum number of this array is the last index number
 
  */
         int max = arr[0];
-        for (int each  : arr ) { // on the right side of the colon, data collection, each represents every single element in the array
+        for (int each : arr) { // on the right side of the colon, data collection, each represents every single element in the array
             // we then compare first element with the next, and then assign if they are greater
             if (each > max) {
                 max = each;
             }
         }
-        System.out.println("Maximum nunber is: "+max);
+        System.out.println("Maximum nunber is: " + max);
 
+    }
+
+
+    public static void arrayMinNum (int [] arr){
+
+        int min = arr[0];
+
+        for ( int each : arr){
+            if(each <min){
+                each = min;
+            }
         }
+        System.out.println("Minimum number is: "+min);
+
+
+    }
 
 }
 
