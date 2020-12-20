82f461b2b015a1c14e3e52820543044696c2994d
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-19 23:59:00 -0600

    commit

--- src/day29_Methods/VoidMethodPractice_1_1.java
+++ src/day29_Methods/VoidMethodPractice_1_1.java
@@ -1,4 +1,29 @@
 package day29_Methods;
 
+/* AM - Access Modifier,
+Specifier
+returnType
+methodName
+(Parameter)
+{ statements to complete the functionality of the method = body
+ */
+
+
 public class VoidMethodPractice_1_1 {
+
+    public static void arrayMaxNum (){
+
+    }
+
 }
+
+
+/*
+task:
+	1. create a function that can print out the maximum number from any given integer array
+
+	2. create a function that can print out the minimum number from any given integer array
+
+	3. create a function that can print the unique elements from the array
+
+ */
