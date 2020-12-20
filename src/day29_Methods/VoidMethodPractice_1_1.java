583537c57aff46025c6f3a04c5fcfefb2261c53b
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-20 00:18:33 -0600

    commit

--- src/day29_Methods/VoidMethodPractice_1_1.java
+++ src/day29_Methods/VoidMethodPractice_1_1.java
@@ -8,12 +8,36 @@ methodName
 { statements to complete the functionality of the method = body
  */
 
+import java.util.Arrays;
 
 public class VoidMethodPractice_1_1 {
 
-    public static void arrayMaxNum (){
+    public static void main(String[] args) {
 
+        int [] a1 ={ 100, -200, 300, 50, 40, 59, 700, 0, 1, 2 };
+
+        arrayMaxNum(a1); // call method and pass the array through the parameter, which then gets assigned to the int max,
+
+        int [] a2 = {1, 2, 3, 4, 5, 6, 800, 9, 0 , 1, 12, 13, 4};
+        arrayMaxNum(a2);
     }
+
+    public static void arrayMaxNum (int []arr){
+/*
+        Arrays.sort(arr); // sorts in ascending order, last index number is the maximum
+        System.out.println("Maximum number: " +arr[arr.length-1]); // maximum number of this array is the last index number
+
+ */
+        int max = arr[0];
+        for (int each  : arr ) { // on the right side of the colon, data collection, each represents every single element in the array
+            // we then compare first element with the next, and then assign if they are greater
+            if (each > max) {
+                max = each;
+            }
+        }
+        System.out.println("Maximum nunber is: "+max);
+
+        }
 
 }
 
