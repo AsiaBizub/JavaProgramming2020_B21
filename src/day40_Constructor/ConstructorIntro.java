3cdb7e81df94e85976b108d00112f3fac17a0cf0
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-12-20 22:56:15 -0600

    commit

--- src/day40_Constructor/ConstructorIntro.java
+++ src/day40_Constructor/ConstructorIntro.java
@@ -1,7 +1,26 @@
 package day40_Constructor;
 
 public class ConstructorIntro {
+
+    static int b;
+
+    public ConstructorIntro(int a) {
+
+        System.out.println("Constructor with int argument");
+        b = 200;
+        this.a = a;
+    }
+
+    int a = 100;
+
     public static void main(String[] args) {
+
         System.out.println("Hello");
+        //System.out.println(a);
+
+        int num = new ConstructorIntro (10).a;
+
+        ConstructorIntro obj2 = new ConstructorIntro(100);
+
     }
 }
