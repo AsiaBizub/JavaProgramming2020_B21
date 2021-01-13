dc0f02faddce46b0ba821b9bf1391c3aa8737fd3
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2021-01-13 15:14:54 -0600

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	src/day47_Abstraction/ShapeTasks/Circle.java
    #	src/day47_Abstraction/ShapeTasks/Shape.java

--- src/day20_ForLoop/SumOFNumbers.java
+++ src/day20_ForLoop/SumOFNumbers.java
@@ -10,6 +10,7 @@ public class SumOFNumbers {
         System.out.println("Enter your number");
         int num = scan.nextInt();  // 100
         scan.close();
+
         int result = 0;
 
         if(num >= 1){
